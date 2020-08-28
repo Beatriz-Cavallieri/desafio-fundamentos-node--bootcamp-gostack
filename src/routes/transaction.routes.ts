4c8b4adc44d25fc578259199fe0ae2b2aad178a1
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-08-27 23:50:06 -0300

    Change from uuidv4 packaage (deprecated) to uuid

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
@@ -17,7 +18,16 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const newTransaction = {
+      id: uuidv4(),
+      title,
+      value,
+      type
+    }
+
+    return response.json(newTransaction)
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
