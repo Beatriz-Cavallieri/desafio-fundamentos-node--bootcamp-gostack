95f7f6ac2059b3be09c76e64ce407334640b8546
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-08-28 00:15:27 -0300

    Add POST method for transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
@@ -20,14 +20,16 @@ transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
 
-    const newTransaction = {
-      id: uuidv4(),
+    const createTransaction = new CreateTransactionService(transactionsRepository);
+
+    const transaction = createTransaction.execute({
       title,
       value,
       type
-    }
+    })
 
-    return response.json(newTransaction)
+    return response.json(transaction)
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
